2efce6c85efb4a6c024a6a3d9715bf7b69dde31e
glopezhe97 <gerlohe97@gmail.com>
2025-07-02 16:57:53 +0200

    large fixed, carrousel added, pending add featured in bbdd, and change isOnSale with featured

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { ProductsServiceService } from '../services/products/products-service.service';
+import { map, Observable, tap } from 'rxjs';
+import { ProductInterface } from '../shared/interfaces/products/product-interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+   public products$!: Observable<ProductInterface[]>;
+   public categories: any;
+ 
+   constructor(private productService: ProductsServiceService) {}
+   
+   ngOnInit(): void {
+     this.products$ = this.productService.getAllProducts('')
+      .pipe(
+        map((products) => 
+          products.filter(product => product.isOnSale === true)),
+          tap(filteredProducts => console.log('Productos en oferta:', filteredProducts))
+      ); // products: fill observable
+     console.log(this.products$);
+   }
 }
